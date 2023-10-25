f0a4b5531d97d4b08cc86854a3cf2384d111311c
Islam <islamka.1994@mail.ru>
2023-10-26 00:36:24 +0300

    add func mul_two_max to widget.py

--- widget.py
+++ widget.py
@@ -1,3 +1,6 @@
+from functools import reduce
+
+
 def disguise_widget(card_info: str) -> str:
     """принимает на вход строку информацией тип карты/счета и номер карты/счета и
     возвращает эту строку с замаскированным номером карты/счета."""
@@ -22,3 +25,12 @@ def new_list(args_list: list) -> list:
     if len(args_list) == 0:
         return args_list
     return [word for word in args_list if len(word) == 0 or word.lower()[0] == word.lower()[-1]]
+
+
+def mul_two_max(numbers_list: list) -> int:
+    """Принимает на вход список целых чисел и возвращает максимальное произведение двух чисел из списка.
+    Если в списке менее двух чисел, возвращает 0"""
+    if len(numbers_list) < 2:
+        return 0
+    else:
+        return reduce(lambda a, b: a * b, sorted([abs(number) for number in numbers_list], reverse=True)[:2])
