3052c3ab0de5a1de0b8e752160b9a6c2a8a7a4b0
Islam <islamka.1994@mail.ru>
2023-10-26 00:13:56 +0300

    add func return_date to widget.py

--- widget.py
+++ widget.py
@@ -9,3 +9,8 @@ def disguise_widget(card_info: str) -> str:
     else:
         card_info_list[-1] = "".join([card_info_list[-1][ind] if ind > -5 else "*" for ind in range(-6, 0)])
     return " ".join(card_info_list)
+
+
+def return_date(date_string: str) -> str:
+    """Принимает строку с данными и возвращает дату в формате dd.mm.yyyy"""
+    return ".".join(date_string.split("T")[0].split("-")[::-1])
