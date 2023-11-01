d481630b9891d05797279b2fdc59149ebcbfd48d
Islam <islamka.1994@mail.ru>
2023-11-02 01:46:30 +0300

    fix func disguise_widget in src/widget.py

--- src/widget.py
+++ src/widget.py
@@ -1,16 +1,16 @@
 from functools import reduce
 
+from src import masks
+
 
 def disguise_widget(card_info: str) -> str:
     """принимает на вход строку информацией тип карты/счета и номер карты/счета и
     возвращает эту строку с замаскированным номером карты/счета."""
     card_info_list = card_info.strip().split()
     if len(card_info_list[-1]) == 16:
-        card_info_list[-1] = " ".join(
-            [card_info_list[-1][:4], card_info_list[-1][4:6] + "**", "****", card_info_list[-1][13:]]
-        )
+        card_info_list[-1] = masks.disguise_card(card_info_list[-1])
     else:
-        card_info_list[-1] = "".join([card_info_list[-1][ind] if ind > -5 else "*" for ind in range(-6, 0)])
+        card_info_list[-1] = masks.disguise_acc_number(card_info_list[-1])
     return " ".join(card_info_list)
 
 
