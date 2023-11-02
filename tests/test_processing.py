414bbae70e74d78a19de318bf82972c63c399707
Islam <islamka.1994@mail.ru>
2023-11-02 03:42:04 +0300

    add test_processing.py to dir tests

---
+++ tests/test_processing.py
@@ -0,0 +1,38 @@
+import pytest
+
+from src.processing import dict_list_by_date, filter_dict_list_by_state
+
+
+@pytest.fixture
+def some_list() -> list:
+    return [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+    ]
+
+
+def test_filter_dict_list_by_state(some_list: list, state: str = "EXECUTED") -> None:
+    assert filter_dict_list_by_state(some_list, state) == [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    ]
+
+
+def test_dict_list_by_date_rev_true(some_list: list, sort_rev: bool = True) -> None:
+    assert dict_list_by_date(some_list, sort_rev) == [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    ]
+
+
+def test_dict_list_by_date_rev_false(some_list: list, sort_rev: bool = False) -> None:
+    assert dict_list_by_date(some_list, sort_rev) == [
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    ]
