712ffadb4290ab47ef055f9bffb5ea266a293727
Islam <islamka.1994@mail.ru>
2023-11-14 03:08:21 +0300

    fix file decorators.py in src

--- src/decorators.py
+++ src/decorators.py
@@ -4,24 +4,29 @@ from typing import Any, Callable
 
 
 def log(filename: None | str = None) -> Callable:
-    def wrapped(func: Callable) -> Callable:
-        @wraps(func)
+    """Функция принимает имя файла или значение None, создает функцию-декоратор wrapped которая принимает другую
+    функцию, проверяет ее через try/catch, возвращает результат этой функции и записывает ее log в filename в случае
+     если передано имя файла, или возвращает ошибку или записывает log ошибки в filename если также в функцию передано
+     имя файла для записи"""
+
+    def wrapped(function: Callable) -> Callable:
+        @wraps(function)
         def inner(*args: Any, **kwargs: dict) -> Any:
             try:
-                func(*args, **kwargs)
+                result = function(*args, **kwargs)
+                log_message = f"{str(datetime.now())[:-7]} {function.__name__} ok\n"
             except Exception as e:
-                result = (
-                    f"{str(datetime.now())[:-7]} {func.__name__} error: <{type(e).__name__}>. "
+                log_message = (
+                    f"{str(datetime.now())[:-7]} {function.__name__} error: <{type(e).__name__}>. "
                     f"Inputs: {args}, {kwargs}\n"
                 )
-            else:
-                result = f"{str(datetime.now())[:-7]} {func.__name__} ok\n"
+                result = None
             if filename is None:
-                return result
+                return log_message
             else:
                 with open(filename, "a", encoding="utf-8") as f:
-                    f.write(result)
-                return func(*args, **kwargs)
+                    f.write(log_message)
+            return result
 
         return inner
 
