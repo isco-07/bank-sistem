835450c1dab88b3c7e2e0449972263ea71bcdc2e
Islam <islamka.1994@mail.ru>
2023-11-16 02:06:02 +0300

    add file utils.py to src

---
+++ src/utils.py
@@ -0,0 +1,21 @@
+import json
+
+
+def deserialization_json_file(json_path: str) -> list:
+    """Функция принимает параметр json_path (имя json файла), возвращает список словарей если файл найден,
+    или пустой список при условии, что файла не найден или поврежден и не соответствует стандарту json"""
+    try:
+        with open(f"../data/{json_path}", "r", encoding="utf-8") as file:
+            return json.load(file)
+    except (json.JSONDecodeError, FileNotFoundError):
+        return []
+
+
+def sum_of_transactions(transaction: dict) -> float | ValueError:
+    """Функция принимает словарь transaction и возвращает результат по ключу amount типа float или
+    ValueError с сообщением 'Транзация выполнена не в рублях. Укажите транзакцию в рублях' если транзакция
+    проведена не в рублях"""
+    if transaction["operationAmount"]["currency"]["code"] == "RUB":
+        return float(transaction["operationAmount"]["amount"])
+    else:
+        return ValueError("Транзация выполнена не в рублях. Укажите транзакцию в рублях")
