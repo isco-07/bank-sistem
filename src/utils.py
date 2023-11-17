f72941c19935b999f0452ac0f71f6921fa416298
Islam <islamka.1994@mail.ru>
2023-11-17 11:15:27 +0300

    fix file utils.py in src

--- src/utils.py
+++ src/utils.py
@@ -1,23 +1,21 @@
 import json
-from typing import Any
 
 
 def deserialization_json_file(json_path: str) -> list:
     """Функция принимает параметр json_path (имя json файла), возвращает список словарей если файл найден,
     или пустой список при условии, что файла не найден или поврежден и не соответствует стандарту json"""
     try:
-        with open(f"../data/{json_path}", "r", encoding="utf-8") as file:
-            return json.load(file)
-    except (json.JSONDecodeError, FileNotFoundError):
+        with open(json_path, encoding="utf-8") as f:
+            return json.load(f)
+    except (json.JSONDecodeError, FileNotFoundError, ValueError):
         return []
 
 
-def sum_of_transactions(transaction: dict) -> float | Any:
+def sum_of_transactions(transaction: dict) -> float | ValueError:
     """Функция принимает словарь transaction и возвращает результат по ключу amount типа float или
     ValueError с сообщением 'Транзация выполнена не в рублях. Укажите транзакцию в рублях' если транзакция
     проведена не в рублях"""
     if transaction["operationAmount"]["currency"]["code"] == "RUB":
         return float(transaction["operationAmount"]["amount"])
     else:
-        print("Транзация выполнена не в рублях. Укажите транзакцию в рублях")
-        return ValueError
+        raise ValueError("Транзация выполнена не в рублях. Укажите транзакцию в рублях")
