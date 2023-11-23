65fac504d95185270cf354a8534ad959899067d2
Islam <islamka.1994@mail.ru>
2023-11-23 16:02:53 +0300

    fix utils.py file in src

--- src/utils.py
+++ src/utils.py
@@ -1,15 +1,6 @@
 import json
-import logging
 
-logger = logging.getLogger("utils_log")
-file_handler = logging.FileHandler("funcs_log.log", "w")
-console_handler = logging.StreamHandler()
-formatter = logging.Formatter("%(asctime)s %(levelname)s: %(message)s")
-file_handler.setFormatter(formatter)
-console_handler.setFormatter(formatter)
-logger.addHandler(file_handler)
-logger.addHandler(console_handler)
-logger.setLevel(level=logging.INFO)
+from src.logger import my_logger
 
 
 def deserialization_json_file(json_path: str) -> list:
@@ -17,10 +8,10 @@ def deserialization_json_file(json_path: str) -> list:
     или пустой список при условии, что файла не найден или поврежден и не соответствует стандарту json"""
     try:
         with open(json_path, encoding="utf-8") as f:
-            logger.info("Operation successful...")
+            my_logger().info("Operation successful...")
             return json.load(f)
     except (json.JSONDecodeError, FileNotFoundError, ValueError):
-        logger.error("The file was not found or could not be processed")
+        my_logger().error("The file was not found or could not be processed")
         return []
 
 
@@ -29,8 +20,8 @@ def sum_of_transactions(transaction: dict) -> float | ValueError:
     ValueError с сообщением 'Транзация выполнена не в рублях. Укажите транзакцию в рублях' если транзакция
     проведена не в рублях"""
     if transaction["operationAmount"]["currency"]["code"] == "RUB":
-        logger.info("The transaction was executed in rubles")
+        my_logger().info("The transaction was executed in rubles")
         return float(transaction["operationAmount"]["amount"])
     else:
-        logger.error("The transaction was not executed in rubles")
+        my_logger().error("The transaction was not executed in rubles")
         raise ValueError("Транзация выполнена не в рублях. Укажите транзакцию в рублях")
