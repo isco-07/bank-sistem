4ba496b4e0b22b8ff6b34784ad1014ae477ac24d
Islam <islamka.1994@mail.ru>
2023-11-25 22:34:58 +0300

    fix test_data_frame.py file in tests

--- tests/test_data_frame.py
+++ tests/test_data_frame.py
@@ -1,24 +1,32 @@
+import os
+
 import pandas as pd
 import pytest
-from src.data_frame import read_xlsx_file, read_csv_file
-import os
-from unittest.mock import Mock
+
+from src.data_frame import read_csv_file, read_xlsx_file
 
 
 @pytest.fixture
-def files_df():
-    df = {'id': [650703.0, 3598919.0, 593027.0],
-          'state': ['EXECUTED', 'EXECUTED', 'CANCELED'],
-          'date': ['2023-09-05T11:30:32Z', '2020-12-06T23:00:58Z', '2023-07-22T05:02:01Z'],
-          'amount': [16210, 29740, 30368],
-          'currency_name': ['Sol', 'Peso', 'Shilling'],
-          'currency_code': ['PEN', 'COP', 'TZS'],
-          'from': ['Счет 58803664561298323391', 'Discover 3172601889670065', 'Visa 1959232722494097'],
-          'to': ['Счет 39745660563456619397', 'Discover 0720428384694643', 'Visa 6804119550473710'],
-          'description': ['Перевод организации', 'Перевод с карты на карту', 'Перевод с карты на карту']}
-    return pd.DataFrame(df)
+def files_df() -> dict:
+    df = {
+        "id": [650703, 3598919, 593027],
+        "state": ["EXECUTED", "EXECUTED", "CANCELED"],
+        "date": ["2023-09-05T11:30:32Z", "2020-12-06T23:00:58Z", "2023-07-22T05:02:01Z"],
+        "amount": [16210, 29740, 30368],
+        "currency_name": ["Sol", "Peso", "Shilling"],
+        "currency_code": ["PEN", "COP", "TZS"],
+        "from": ["Счет 58803664561298323391", "Discover 3172601889670065", "Visa 1959232722494097"],
+        "to": ["Счет 39745660563456619397", "Discover 0720428384694643", "Visa 6804119550473710"],
+        "description": ["Перевод организации", "Перевод с карты на карту", "Перевод с карты на карту"],
+    }
+    return pd.DataFrame(df).to_dict()
 
 
-def test_read_csv_file(files_df) -> None:
-    work_path = os.path.join(os.path.dirname(__file__), '..', 'data', 'transactions.csv')
-    assert read_csv_file(work_path) == files_df
+def test_read_csv_file(files_df: dict) -> None:
+    work_path = os.path.join(os.path.dirname(__file__), "..", "data", "transactions.csv")
+    assert read_csv_file(work_path).iloc[:3].to_dict() == files_df
+
+
+def test_read_xlsx_file(files_df: dict) -> None:
+    work_path = os.path.join(os.path.dirname(__file__), "..", "data", "transactions_excel.xlsx")
+    assert read_xlsx_file(work_path).iloc[:3].to_dict() == files_df
